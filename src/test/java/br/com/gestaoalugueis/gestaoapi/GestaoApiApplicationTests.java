323d0f3cd7421ee1819917cfa8bb90c52b0e7b04
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-11-06 19:07:09 -0300

    Commit inicial: Estrutura do projeto Spring Boot (Java 21 + Postgres)

---
+++ src/test/java/br/com/gestaoalugueis/gestaoapi/GestaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestaoalugueis.gestaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
