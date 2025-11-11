5abcb538760a0480c9ddd5eef7014f0e3742506f
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-11-11 16:51:52 -0300

    Feature: Cria LocacaoRequestDTO

---
+++ src/main/java/br/com/gestaoalugueis/gestaoapi/dto/LocacaoRequestDTO.java
@@ -0,0 +1,16 @@
+package br.com.gestaoalugueis.gestaoapi.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record LocacaoRequestDTO(
+        BigDecimal valor_aluguel,
+        Integer dia_vencimento,
+        LocalDate data_inico_contrato,
+        Integer duracao_meses,
+        //IDs das entidades que já existem
+        UUID imovelId,
+        UUID inquilinoId
+) {
+}
