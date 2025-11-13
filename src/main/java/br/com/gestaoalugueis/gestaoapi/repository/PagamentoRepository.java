74862247052dabf30dd7207684f5004501186232
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-11-13 19:55:44 -0300

    Refactor: Adiciona query customizada para cancelamento de Pagamentos

--- src/main/java/br/com/gestaoalugueis/gestaoapi/repository/PagamentoRepository.java
+++ src/main/java/br/com/gestaoalugueis/gestaoapi/repository/PagamentoRepository.java
@@ -1,9 +1,20 @@
 package br.com.gestaoalugueis.gestaoapi.repository;
 
+import br.com.gestaoalugueis.gestaoapi.entity.Locacao;
 import br.com.gestaoalugueis.gestaoapi.entity.Pagamento;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.UUID;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
+
+    @Transactional
+    @Modifying // esta anotação indica que a query modifica os dados(DELETE/UPDATE)
+    @Query("DELETE FROM Pagamento p WHERE p.locacao = :locacao AND p.statusPagamentos <> 'PAGO'")
+    void deleteFuturePaymentsByLocacao(@Param("locacao") Locacao locacao);
+
 }
--- src/main/java/br/com/gestaoalugueis/gestaoapi/service/LocacaoService.java
+++ src/main/java/br/com/gestaoalugueis/gestaoapi/service/LocacaoService.java
@@ -1,11 +1,14 @@
 package br.com.gestaoalugueis.gestaoapi.service;
 
 import br.com.gestaoalugueis.gestaoapi.dto.LocacaoRequestDTO;
+import br.com.gestaoalugueis.gestaoapi.entity.Imovel;
+import br.com.gestaoalugueis.gestaoapi.entity.Inquilino;
 import br.com.gestaoalugueis.gestaoapi.entity.Locacao;
 import br.com.gestaoalugueis.gestaoapi.enums.Status;
 import br.com.gestaoalugueis.gestaoapi.repository.ImovelRepository;
 import br.com.gestaoalugueis.gestaoapi.repository.InquilinoRepository;
 import br.com.gestaoalugueis.gestaoapi.repository.LocacaoRepository;
+import br.com.gestaoalugueis.gestaoapi.repository.PagamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +22,7 @@ public class LocacaoService {
     private final LocacaoRepository locacaoRepository;
     private final InquilinoRepository inquilinoRepository;
     private final ImovelRepository imovelRepository;
+    private final PagamentoRepository pagamentoRepository;
 
     public Locacao criarLocacao(LocacaoRequestDTO dto) {
         var inquilinoDaLocacao = inquilinoRepository.findById(dto.inquilinoId())
@@ -46,5 +50,32 @@ public class LocacaoService {
         return locacaoRepository
                 .findById(id)
                 .orElseThrow(() -> new RuntimeException("Locação não econtrada."));
+    }
+
+    public void deletarLocacaoPorId(UUID id) {
+        Locacao locacaoInativa = buscarLocacaoPorId(id);
+        pagamentoRepository.deleteFuturePaymentsByLocacao(locacaoInativa); // cancela os pagamentos futuros
+        locacaoInativa.setStatus(Status.INATIVO);
+        locacaoRepository.save(locacaoInativa);
+    }
+
+    public Locacao atualizarLocacao(UUID id, LocacaoRequestDTO dto) {
+        Locacao locacaoAtualizado = buscarLocacaoPorId(id);
+
+        Imovel imovelBuscado = imovelRepository.findById(dto.imovelId())
+                        .orElseThrow(() -> new RuntimeException("Imóvel não encontrado para associação."));
+
+        Inquilino inquilinoBuscado = inquilinoRepository.findById(dto.inquilinoId())
+                        .orElseThrow(() -> new RuntimeException("Inquilino não encontrado para associação."));
+
+        locacaoAtualizado.setValor_aluguel(dto.valor_aluguel());
+        locacaoAtualizado.setDia_vencimento(dto.dia_vencimento());
+        locacaoAtualizado.setData_inicio_contrato(dto.data_inico_contrato());
+        locacaoAtualizado.setDuracao_meses(dto.duracao_meses());
+
+        locacaoAtualizado.setImovel(imovelBuscado);
+        locacaoAtualizado.setInquilino(inquilinoBuscado);
+
+        return locacaoRepository.save(locacaoAtualizado);
     }
 }
