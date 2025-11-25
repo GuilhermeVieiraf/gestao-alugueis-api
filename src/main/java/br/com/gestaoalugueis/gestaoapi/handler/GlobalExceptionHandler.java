c540bb34160d29b7ffc35174ef100dc63dd099ec
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-11-25 16:17:48 -0300

    Feature: Implementa GlobalExceptionHandler (Tratamento de Erros 400, 404 e 409)

--- src/main/java/br/com/gestaoalugueis/gestaoapi/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/gestaoalugueis/gestaoapi/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.gestaoalugueis.gestaoapi.handler;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -21,6 +22,17 @@ public class GlobalExceptionHandler {
                 .collect(Collectors.toList()); // junta tudo em uma nova lista
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleNotFoundException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex){
+        String rootCause = ex.getRootCause().getMessage();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Erro de integridade: " + rootCause);
     }
 
 }
