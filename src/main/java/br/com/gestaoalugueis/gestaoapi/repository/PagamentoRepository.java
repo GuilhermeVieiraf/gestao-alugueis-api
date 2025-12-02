b70b6008a4e30432b75d9ac688eae06c7dc5e6c7
GuilhermeVieiraf <guilhermevfelix06@gmail.com>
2025-12-02 19:30:54 -0300

    Feature: Implementa rota manual para validação de vencimentos e finaliza PagamentoController

--- src/main/java/br/com/gestaoalugueis/gestaoapi/repository/PagamentoRepository.java
+++ src/main/java/br/com/gestaoalugueis/gestaoapi/repository/PagamentoRepository.java
@@ -2,16 +2,24 @@ package br.com.gestaoalugueis.gestaoapi.repository;
 
 import br.com.gestaoalugueis.gestaoapi.entity.Locacao;
 import br.com.gestaoalugueis.gestaoapi.entity.Pagamento;
-import jakarta.transaction.Transactional;
+import br.com.gestaoalugueis.gestaoapi.enums.StatusPagamento;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface PagamentoRepository extends JpaRepository<Pagamento, UUID> {
+
+    List<Pagamento> findByStatusPagamentoAndDataVencimentoLessThanEqual(
+            StatusPagamento status,
+            LocalDate dataLimete
+    );
 
     @Transactional
     @Modifying // esta anotação indica que a query modifica os dados(DELETE/UPDATE)
